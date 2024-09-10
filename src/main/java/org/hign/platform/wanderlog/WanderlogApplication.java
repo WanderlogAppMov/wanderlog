caa4d53ab166011603c2e880e8814eef2cd306fc
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-09-10 00:25:20 -0500

    Primera versión del proyecto wanderlog

---
+++ src/main/java/org/hign/platform/wanderlog/WanderlogApplication.java
@@ -0,0 +1,13 @@
+package org.hign.platform.wanderlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanderlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hign/platform/wanderlog/WanderlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hign.platform.wanderlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
