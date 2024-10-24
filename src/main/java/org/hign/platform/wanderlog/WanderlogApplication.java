2e1547819a7837386fb0934f13ea963a2b1c6ab3
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-10-23 23:00:13 -0500

    feat: added shared context.

--- src/main/java/org/hign/platform/wanderlog/WanderlogApplication.java
+++ src/main/java/org/hign/platform/wanderlog/WanderlogApplication.java
@@ -4,21 +4,23 @@ import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WanderlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WanderlogApplication.class, args);
     }
 
-    @Bean
+   /* @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new io.swagger.v3.oas.models.info.Info()
                         .title("Wanderlog API")
                         .version("1.0")
                         .description("Wanderlog API"));
-    }
+    }*/
 
 }
