9c2dfdf4359d9c51a9b79f15747547ed52e29d4a
Diego Flores Avalos <129472574+DiegoFA04@users.noreply.github.com>
2024-11-09 20:40:28 -0500

    feat(iam): fix roles.

--- src/main/java/org/hign/platform/wanderlog/iam/domain/model/valueobjects/Roles.java
+++ src/main/java/org/hign/platform/wanderlog/iam/domain/model/valueobjects/Roles.java
@@ -1,7 +1,7 @@
 package org.hign.platform.wanderlog.iam.domain.model.valueobjects;
 
 public enum Roles {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_COMPANY
+    ROLE_TRAVELER,
+    ROLE_AGENCY
+
 }
